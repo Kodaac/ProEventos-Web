7b9f7eee95319438335090844fd915b8f4e56f8a
Kodaac <nogueira.hs@hotmail.com>
2023-10-13 19:05:45 -0300

    Barra de navegação e grid de eventos

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,30 @@ import { Component } from '@angular/core';
 })
 export class EventosComponent {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+
+  widthImg: number = 150;
+  marginImg: number = 2;
+  exibirImagem: boolean = true;
+  private _filtroLista: string = '';
+
+  public get filtroLista(): string{
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 || 
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
 
   constructor(private http: HttpClient){
 
@@ -18,13 +41,18 @@ export class EventosComponent {
     this.getEventos();
   }
 
-  public getEventos(): void{
+  alterarImagem(){
+    this.exibirImagem = !this.exibirImagem;
+  }
 
+  public getEventos(): void{
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error)
     );
-    
   }
 
 }
